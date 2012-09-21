675bacce9ec9aeabd8f97e8c08795d5326090fd0
Big Angry Blue Fish <babf@cpan.org>
2012-09-21 15:15:31 +0000

    Import of BABF/Text-CSV-Slurp-1.0 from CPAN.
    gitpan-cpan-distribution: Text-CSV-Slurp
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         BABF/Text-CSV-Slurp-1.0.tar.gz
    gitpan-cpan-author:       BABF
    gitpan-cpan-maturity:     released

--- lib/Text/CSV/Slurp.pm
+++ lib/Text/CSV/Slurp.pm
@@ -4,11 +4,11 @@ use strict;
 use warnings;
 
 use Text::CSV;
-use IO::Handle;
+use IO::File;
 
 use vars qw/$VERSION/;
 
-$VERSION = 0.9;
+$VERSION = 1.0;
 
 sub new {
   my $class = shift;
@@ -32,7 +32,7 @@ sub load {
     return _from_handle($io,\%opt);
   }
   elsif (defined $opt{file}) {
-    my $io = new IO::Handle;
+    my $io = new IO::File;
     open($io, "<$opt{file}") || die "Could not open $opt{file} $!";
     delete $opt{file};
     return _from_handle($io,\%opt);
@@ -190,14 +190,14 @@ to determine the fields and order in which they appear in the CSV. For example:
 
  my $csv = Text::CSV::Slurp->create( input => \@array_of_hashes, field_order => ['one','three','two'] );
 
-If field_order is not supplied then the sorted keys of the first hash in the 
+If field_order is not supplied then the sorted keys of the first hash in the
 input are used instead.
 
 =head1 DEPENDENCIES
 
 L<Text::CSV>
 
-L<IO::Handle>
+L<IO::File>
 
 L<Test::Most> - for tests only
 
